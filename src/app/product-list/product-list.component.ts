7ca6f7ae995a54d7f402352960fca2d888732f1a
samicey <samuelchibuike22@gmail.com>
2019-11-19 14:20:22 +0100

    3 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
+      pageTitle: string ='Product list';
   constructor() { }
 
   ngOnInit() {
