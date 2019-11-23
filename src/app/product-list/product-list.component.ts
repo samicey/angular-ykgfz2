baea3f9db20060256f7a37b0c9ffc63986114b5f
samicey <samuelchibuike22@gmail.com>
2019-11-23 15:27:43 +0000

    added an interface for the products

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IProduct} from './iproduct'
 
 @Component({
   selector: 'app-product-list',
@@ -9,13 +10,13 @@ export class ProductListComponent implements OnInit {
       pageTitle: string ='Product list';
       showImage: boolean = false;
       listFilter: string ='cart';
-      products: any[] = [
+      products: IProduct[] = [
   {
     "productId": 1,
     "productName": "Leaf Rake",
     "productCode": "GDN-0011",
     "releaseDate": "March 19, 2019",
-    "description": "Leaf rake with 48-inch wooden handle.",
+    "description": "L  eaf rake with 48-inch wooden handle.",
     "price": 19.95,
     "starRating": 3.2,
     "imageUrl": "assets/images/leaf_rake.png"
