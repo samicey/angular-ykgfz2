3e25ad23afe04e3a559d08c317ed574f4e0a2925
samicey <samuelchibuike22@gmail.com>
2019-11-27 14:34:05 +0100

    implemented routing for the application to switch between components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
 import {ConvertToSpacePipe} from './shared/convert-to-space-pipe';
 
 import { AppComponent } from './app.component';
@@ -10,10 +11,20 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { StarComponent } from './shared/star.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,HttpClientModule],
-  declarations: [ AppComponent, HelloComponent, ProductListComponent,ConvertToSpacePipe,StarComponent, WelcomeComponent, ProductDetailComponent ],
+  imports:      [ BrowserModule, 
+                  FormsModule,
+                  HttpClientModule,
+                  RouterModule.forRoot([{path:'products', component:ProductListComponent},
+                  {path:'products/id', component:ProductDetailComponent},
+                  {path:'welcome', component:WelcomeComponent},
+                  {path:'',redirectTo:'welcome', pathMatch:'full'},
+                  {path:'**', component:PageNotFoundComponent}
+                  ])
+                  ],
+  declarations: [ AppComponent, HelloComponent, ProductListComponent,ConvertToSpacePipe,StarComponent, WelcomeComponent, ProductDetailComponent, PageNotFoundComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
